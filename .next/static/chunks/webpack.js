824e4d383f2306b98fca6a9170f0d03f302f764a
TuanDatCoder <tuandatdq03@gmail.com>
2026-05-06 23:20:06 +0700

    Fix: Install @tailwindcss/postcss and upgrade configurations

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b0355dd3fc98e0c4"; }
+/******/ 		__webpack_require__.h = function() { return "db880c51a64edd8d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
