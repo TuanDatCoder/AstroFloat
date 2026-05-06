d5796162938aea4659d34ced5a69d0e6264c92c4
TuanDatCoder <tuandatdq03@gmail.com>
2026-05-06 23:28:44 +0700

    Config: Set Vercel framework to nextjs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce48475352b9a1a5"; }
+/******/ 		__webpack_require__.h = function() { return "8c8cac3d14f6a1a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
